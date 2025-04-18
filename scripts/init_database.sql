6e4e56ec1eda17485c97def9b01b499d7db4e2cc
Erdin Nurjaman <111257907+erdinnurjaman@users.noreply.github.com>
2025-04-18 16:49:41 +0700

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,19 @@
+/*
+========================================================================
+Create Database and Schemas
+========================================================================
+Script Purpose:
+    abxjab
+WARNING:
+    JHJASBXKUB
+*/
+
+-- Create the Data Warehouse database
+CREATE DATABASE DataWarehouse;
+
+USE datawarehouse;
+
+-- Create schemas
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
