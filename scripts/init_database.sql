ee80fbe95571422843d87636cab703b4fa7f29ef
Erdin Nurjaman <111257907+erdinnurjaman@users.noreply.github.com>
2025-04-22 15:45:04 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,11 +1,16 @@
 /*
-========================================================================
+=============================================================
 Create Database and Schemas
-========================================================================
+=============================================================
 Script Purpose:
-    abxjab
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
 WARNING:
-    JHJASBXKUB
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
 */
 
 -- Create the Data Warehouse database
